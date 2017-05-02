1aa6a977d3efa605f943bfb4ec6d7bc3db6c0ae3
Skdi <jddhghf@gmail.com>
2017-05-02 13:35:51 -0500

    Create claselab2_5.cpp

---
+++ claselab2_5.cpp
@@ -0,0 +1,23 @@
+cada interface tiene porlomenos una direccion mac(direccion fisica)
+ip 4 elementos , cada elemento de 1byte ejemplo 74.125.141.94 ejemplo google
+clases de IP:
+  Clase A :/ NETID-/ HostID  / HostID  / HostID  /
+            10.0.52.0 (ejemplo)(empresas grandes)  google      
+           teoricamente tiene 255redes / 2¨24hosts: 1millon°
+           clase A 10-126(NETID)
+           mascara de red  :255.0.0.0
+           
+  Clase B: / NETID/  NETID /  HostID / HostID  /
+            instituciones educativas
+            127.18.5.1
+            wwww.unsa.edu.pe
+            mascara de red  :255.255.0.0
+  
+  Clase C:  /  NETID  /  NETID /  NETID  /  HostID  /
+            192.168.1.2 (redes pequeñas)
+            www.unicef.com
+            mascara de red  :255.255.255.0
+            
+            
+            
+           
