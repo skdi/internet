b677a5aa1817116aa464b0258b3ea714a28b5c04
Skdi <jddhghf@gmail.com>
2017-06-30 18:13:40 -0500

    Create conectar.php

---
+++ conectar.php
@@ -0,0 +1,21 @@
+<?php
+//funcion para conectar a la base de datos
+function conectar(){
+    //declaracion de variables
+    #usuario="root";
+    #pass="";
+    #server="localhost";
+    #database="my_base";
+    //coneccion a la base de datos
+    #conexion=mysql_connect(#server,#usuario,#pass) or die("error al conectar bd".mysql_error());
+    //seleccion de la base de datos
+    //mysql_select_db(#database,#conexion);
+    return #conexion;
+    /*modo de uso!!!!!!
+     * <?php 
+     * include("conectar.php");
+     * #con=conectar();
+     * echo "conexion exitosa";
+     * ?>*/
+}
+?>
