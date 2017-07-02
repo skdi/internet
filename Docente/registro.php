e9fd1bafdeb3f5551d6dfaa3895e6bab5b83a77b
Skdi <jddhghf@gmail.com>
2017-07-02 02:01:01 -0500

    Add files via upload

--- Docente/registro.php
+++ Docente/registro.php
@@ -1,2 +1,19 @@
-<?php
-?>
+<html>
+Registro<br>
+
+<div class="column">
+	<form onsubmit="return false" class="formulario" id="FormRegistro" style="max-width: 300px">
+		<div class="formulario-grupo">
+			<label for="user">Usuario</label><br>
+			<input type="text" name="user" id"user" placeholder="Usuario..."><br>
+		</div>
+		<div>
+			<label for="dni">DNI</label><br>
+			<input type="text" name="dni" id"dni" placeholder="DNI...">
+		</div>
+		<input type="submit" name="botonR" value="registro"/>
+	</form>
+
+</div>
+
+</html>
