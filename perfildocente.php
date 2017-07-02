5435792c7d7e35ec7f6207dcb50a6548635cb039
Skdi <jddhghf@gmail.com>
2017-07-01 19:53:24 -0500

    Update and rename perfildocente.php to lista_formuladoresphp

--- perfildocente.php
+++
@@ -1,6 +0,0 @@
-<?php /*perfil docente*/
-     include("conectar.php");
-     #cnx=conectar();
-     #selectBD=mysql_select_db('usuario',cnx) or die("problema con la bd seleccionada");
-     #busqueda=#
-?>
