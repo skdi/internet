8d52de347f8a78968bb32f1f57db890edf43f739
Skdi <jddhghf@gmail.com>
2017-07-01 20:26:18 -0500

    Update lista_formuladores.php

--- Docente/lista_formuladores.php
+++ Docente/lista_formuladores.php
@@ -1,5 +1,5 @@
 <?php /*lista_formuladores*/
-     include("conectar.php");
+     include("../conectar.php");
      #resultado=mysql_query("SELECT * from nombretabla");
      while(#file=mysql_fetch_array(#resultado)){
           echo " #file[Nombre]."|".#file[Apellido]."|".#file[Codigo] <br>";
