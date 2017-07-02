4ce2dbc84dfc3ed404d649f09600bdb98be4f678
Skdi <jddhghf@gmail.com>
2017-07-01 22:18:08 -0500

    Update lista_controladores.php

--- Docente/lista_controladores.php
+++ Docente/lista_controladores.php
@@ -2,7 +2,7 @@
      include("../conectar.php");
      #resultado=mysql_query("SELECT * from nombretabla");
      while(#file=mysql_fetch_array(#resultado)){
-          echo " #file[Nombre]."|".#file[Apellido]."|".#file[Codigo] <br>";
+          echo " #file[Nombre] | #file[Apellido] | #file[Codigo] <br>";
      }
      
 ?>
