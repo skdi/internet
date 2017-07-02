568dfe9070d49eb03e7e3337e941db8d73ddd6ec
Skdi <jddhghf@gmail.com>
2017-07-02 11:07:09 -0500

    Delete lista_controladores.php

--- Docente/lista_controladores.php
+++
@@ -1,8 +0,0 @@
-<?php /*lista_controladores*/
-     include("../conectar.php");
-     #resultado=mysql_query("SELECT * from nombretabla");
-     while(#file=mysql_fetch_array(#resultado)){
-          echo " #file[Nombre] | #file[Apellido] | #file[Codigo] <br>";
-     }
-     
-?>
