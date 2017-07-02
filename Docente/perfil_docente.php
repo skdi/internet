31c749f84116d96c0baf5877c6f8055b8ffa3b59
Skdi <jddhghf@gmail.com>
2017-07-01 20:59:53 -0500

    Update perfil_docente.php

--- Docente/perfil_docente.php
+++ Docente/perfil_docente.php
@@ -1,2 +1,14 @@
+
+<div id="perfil_docente">
+		<h1>Perfil Docente</h1>
+			<form>
+				<li> Nombre</li>
+				<input type="text" name="nombre"/>
+				<li> Apellidos</li>
+				<input type="text" name="apellidos"/>
+				<input type="submit" name="boton" value="login"/>
+				
+			</form>
+		</div>
 <?php
 ?>
