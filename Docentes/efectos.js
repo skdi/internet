6b937f59348fc46f9b44b7b3da2c4a4658650453
Skdi <jddhghf@gmail.com>
2017-07-02 13:34:55 -0500

    Add files via upload

---
+++ Docentes/efectos.js
@@ -0,0 +1,12 @@
+function registro(usuario,nombre,pass1,pass2){
+	if(limpiar(usuario)!='' && limpiar(nombre)!='' && limpiar(pass1)!='' && limpiar(pass2)!='')
+		if(limpiar(pass1)!=limpiar(pass2)){
+			$("error").type("text");
+			$("#error").text("las contraseñas no coinciden");
+		}
+
+}
+function limpiar(valor){
+	var cadena=valor.split(" ").join("");
+	return cadena;
+}
