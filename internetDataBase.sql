909d717c70d18186f49b2f8e8ec1e39aa01ffff1
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2017-07-08 22:15:20 -0500

    Update internetDataBase.sql

--- internetDataBase.sql
+++ internetDataBase.sql
@@ -1,164 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-07-2017 a las 05:13:20
+-- Versión del servidor: 10.1.24-MariaDB
+-- Versión de PHP: 7.1.6
 
-CREATE TABLE procesos (
-                id INT AUTO_INCREMENT NOT NULL,
-                fecha DATE NOT NULL,
-                nombre VARCHAR(100) NOT NULL,
-                PRIMARY KEY (id)
-);
-
-
-CREATE TABLE participantes_procesos (
-                id INT NOT NULL,
-                proceso_id INT NOT NULL,
-                Escuela VARCHAR(100) NOT NULL,
-                area VARCHAR(20) NOT NULL,
-                id_participante VARCHAR NOT NULL,
-                area VARCHAR NOT NULL,
-                escuela VARCHAR NOT NULL,
-                aula VARCHAR NOT NULL,
-                PRIMARY KEY (id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE participantes (
-                id INT NOT NULL,
-                dni VARCHAR(8) NOT NULL,
-                nombres VARCHAR(100) NOT NULL,
-                apellidos VARCHAR(100) NOT NULL,
-                codigopostulante VARCHAR(100) NOT NULL,
-                tipo_participante VARCHAR(100) NOT NULL,
-                estado BOOLEAN NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `proyecto`
+--
 
-CREATE TABLE proceso_pregunta (
-                id INT NOT NULL,
-                id_participante VARCHAR NOT NULL,
-                id_proceso VARCHAR NOT NULL,
-                id_pregunta VARCHAR NOT NULL,
-                estado VARCHAR NOT NULL,
-                area VARCHAR NOT NULL,
-                PRIMARY KEY (id)
-);
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `area`
+--
 
-CREATE TABLE area (
-                id INT NOT NULL,
-                PRIMARY KEY (id)
-);
+CREATE TABLE `area` (
+  `area` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-CREATE TABLE pregunta (
-                id INT NOT NULL,
-                titulo VARCHAR(100) NOT NULL,
-                descripcion VARCHAR(500) NOT NULL,
-                examen_id INT NOT NULL,
-                respuesta VARCHAR(1) NOT NULL,
-                respuestaImagen VARCHAR NOT NULL,
-                PRIMARY KEY (id)
-);
+--
+-- Estructura de tabla para la tabla `participante`
+--
 
+CREATE TABLE `participante` (
+  `id` int(11) NOT NULL,
+  `dni` varchar(100) DEFAULT NULL,
+  `apellidos` varchar(100) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `dependencia` varchar(100) DEFAULT NULL,
+  `facultad` varchar(100) DEFAULT NULL,
+  `telefono` int(11) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `categoria` varchar(100) DEFAULT NULL,
+  `regimen` varchar(100) DEFAULT NULL,
+  `cargo` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Escuela (
-                nombre VARCHAR NOT NULL
-);
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `tipo_participante`
+--
 
-CREATE TABLE aula (
-                id INT NOT NULL,
-                facultad VARCHAR NOT NULL,
-                PRIMARY KEY (id)
-);
+CREATE TABLE `tipo_participante` (
+  `cargo` varchar(50) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `funcion` tinytext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-CREATE TABLE Tipo_Participante (
-                nombre VARCHAR(100) NOT NULL
-);
+--
+-- Estructura de tabla para la tabla `usuario`
+--
 
+CREATE TABLE `usuario` (
+  `dni` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `con` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE participantes_procesos ADD CONSTRAINT procesos_participantes_procesos_fk
-FOREIGN KEY (proceso_id)
-REFERENCES procesos (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Índices para tablas volcadas
+--
 
-ALTER TABLE pregunta ADD CONSTRAINT procesos_preguntas_fk
-FOREIGN KEY (id)
-REFERENCES procesos (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Indices de la tabla `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`area`);
 
-ALTER TABLE proceso_pregunta ADD CONSTRAINT procesos_proceso_pregunta_fk
-FOREIGN KEY (id)
-REFERENCES procesos (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Indices de la tabla `participante`
+--
+ALTER TABLE `participante`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cargo` (`cargo`);
 
-ALTER TABLE participantes ADD CONSTRAINT participantes_procesos_participantes_fk
-FOREIGN KEY (id)
-REFERENCES participantes_procesos (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Indices de la tabla `tipo_participante`
+--
+ALTER TABLE `tipo_participante`
+  ADD PRIMARY KEY (`cargo`);
 
-/*
-Warning: Relationship has no columns to map:
-*/
-ALTER TABLE Tipo_Participante ADD CONSTRAINT participantes_tipo_participante_fk
-FOREIGN KEY ()
-REFERENCES participantes ()
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`dni`);
 
-/*
-Warning: Relationship has no columns to map:
-*/
-ALTER TABLE proceso_pregunta ADD CONSTRAINT participantes_proceso_pregunta_fk
-FOREIGN KEY ()
-REFERENCES participantes ()
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
-/*
-Warning: Relationship has no columns to map:
-*/
-ALTER TABLE area ADD CONSTRAINT proceso_pregunta_area_fk
-FOREIGN KEY ()
-REFERENCES proceso_pregunta ()
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- AUTO_INCREMENT de la tabla `participante`
+--
+ALTER TABLE `participante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
 
-ALTER TABLE pregunta ADD CONSTRAINT proceso_pregunta_pregunta_fk
-FOREIGN KEY (id)
-REFERENCES proceso_pregunta (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-/*
-Warning: Relationship has no columns to map:
-*/
-ALTER TABLE Escuela ADD CONSTRAINT area_escuela_fk
-FOREIGN KEY ()
-REFERENCES area ()
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE participantes_procesos ADD CONSTRAINT area_participantes_procesos_fk
-FOREIGN KEY (id)
-REFERENCES area (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE pregunta ADD CONSTRAINT area_pregunta_fk
-FOREIGN KEY (id)
-REFERENCES area (id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-/*
-Warning: Relationship has no columns to map:
-*/
-ALTER TABLE aula ADD CONSTRAINT escuela_aula_fk
-FOREIGN KEY ()
-REFERENCES Escuela ()
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+--
+-- Filtros para la tabla `participante`
+--
+ALTER TABLE `participante`
+  ADD CONSTRAINT `participante_ibfk_1` FOREIGN KEY (`cargo`) REFERENCES `tipo_participante` (`cargo`);
+COMMIT;
