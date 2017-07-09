3b686f585adade99f104e16bc4eb5c5e26f5e18e
Skdi <jddhghf@gmail.com>
2017-07-09 01:57:05 -0500

    Update modifica.php

--- sistema_web_cascara/php/modifica.php
+++ sistema_web_cascara/php/modifica.php
@@ -9,18 +9,18 @@
 	$regimen = $_POST['regimen'];
 	//Facultad	Categoria	Regimen	Cargo
 
-		if($regimen){//si es docente
-		mysqli_query("UPDATE $participante set regimen='$regimen' WHERE nombre='$nombre' AND  apellido='$apellido'");
+		if($regimen){
+		mysqli_query($con,"UPDATE participante set regimen='$regimen' WHERE nombre='$nombre' AND  apellido='$apellido'");
 	}
-		if($cargo){//si es docente
-		mysqli_query("UPDATE $participante set cargo='$cargo' WHERE nombre='$nombre' AND  apellido='$apellido'");
+		if($cargo){
+		mysqli_query($con,"UPDATE participante set cargo='$cargo' WHERE nombre='$nombre' AND  apellido='$apellido'");
 	}
-		if($facultad){//si es docente
-		mysqli_query("UPDATE $participante set facultad='$facultad' WHERE nombre='$nombre' AND  apellido='$apellido'");
+		if($facultad){
+		mysqli_query($con,"UPDATE participante set facultad='$facultad' WHERE nombre='$nombre' AND  apellido='$apellido'");
 	}
-		if($dependencia){//si es docente
-		mysqli_query("UPDATE $participante set dependencia='$dependencia' WHERE nombre='$nombre' AND  apellido='$apellido'");
+		if($dependencia){
+		mysqli_query($con,"UPDATE participante set dependencia='$dependencia' WHERE nombre='$nombre' AND  apellido='$apellido'");
 	}
 
-}
+
 ?>
