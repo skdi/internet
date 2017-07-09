99c129a95c23c911c0e06f4fcebe442b19113c72
RaulToto <djbrexman@gmail.com>
2017-07-09 03:25:33 -0500

    update data base

--- internetDataBase.sql
+++ internetDataBase.sql
@@ -1,311 +1,178 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-07-2017 a las 08:54:11
--- Versión del servidor: 10.1.24-MariaDB
--- Versión de PHP: 7.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE TABLE proceso (
+                fecha DATE NOT NULL,
+                nombre VARCHAR(100) NOT NULL,
+                PRIMARY KEY (fecha)
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE detalle_participante (
+                id_detalle_participante INT NOT NULL,
+                facultad VARCHAR(100) NOT NULL,
+                regimen VARCHAR(100) NOT NULL,
+                dependencia VARCHAR(100) NOT NULL,
+                PRIMARY KEY (id_detalle_participante)
+);
 
---
--- Base de datos: `proyecto`
---
 
--- --------------------------------------------------------
+CREATE TABLE tipo_participante (
+                id_tipo_participante INT NOT NULL,
+                nombre VARCHAR(100) NOT NULL,
+                funcion VARCHAR(100) NOT NULL,
+                PRIMARY KEY (id_tipo_participante)
+);
 
---
--- Estructura de tabla para la tabla `area`
---
 
-CREATE TABLE `area` (
-  `area` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE participante (
+                dni VARCHAR(8) NOT NULL,
+                nombre VARCHAR(100) NOT NULL,
+                apellido VARCHAR(100) NOT NULL,
+                id_tipo_participante INT NOT NULL,
+                id_detalle_participante INT NOT NULL,
+                id_participante_proceso INT NOT NULL,
+                fecha DATE NOT NULL,
+                PRIMARY KEY (dni)
+);
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `aula`
---
+CREATE TABLE proceso_pregunta (
+                id_proceso_pregunta INT NOT NULL,
+                estado BOOLEAN NOT NULL,
+                dni VARCHAR(8) NOT NULL,
+                fecha DATE NOT NULL,
+                PRIMARY KEY (id_proceso_pregunta)
+);
 
-CREATE TABLE `aula` (
-  `numero` int(11) NOT NULL,
-  `escuela` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+CREATE TABLE area (
+                nombre VARCHAR(20) NOT NULL,
+                id_proceso_pregunta INT NOT NULL,
+                PRIMARY KEY (nombre)
+);
 
---
--- Estructura de tabla para la tabla `escuela`
---
 
-CREATE TABLE `escuela` (
-  `nombre` varchar(255) NOT NULL,
-  `area` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE participante_proceso (
+                id_participante_proceso INT NOT NULL,
+                escuela VARCHAR NOT NULL,
+                area VARCHAR NOT NULL,
+                aula INT NOT NULL,
+                fecha DATE NOT NULL,
+                nombre VARCHAR(20) NOT NULL,
+                PRIMARY KEY (id_participante_proceso)
+);
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `participante`
---
+CREATE TABLE escuela (
+                id_escuela INT NOT NULL,
+                nombre VARCHAR(30) NOT NULL,
+                area_nombre VARCHAR(20) NOT NULL,
+                PRIMARY KEY (id_escuela)
+);
 
-CREATE TABLE `participante` (
-  `id` int(11) NOT NULL,
-  `tipo` char(1) NOT NULL,
-  `dni` varchar(100) DEFAULT NULL,
-  `apellidos` varchar(100) DEFAULT NULL,
-  `nombre` varchar(100) DEFAULT NULL,
-  `dependencia` varchar(100) DEFAULT NULL,
-  `facultad` varchar(100) DEFAULT NULL,
-  `telefono` int(11) DEFAULT NULL,
-  `correo` varchar(100) DEFAULT NULL,
-  `categoria` varchar(100) DEFAULT NULL,
-  `regimen` varchar(100) DEFAULT NULL,
-  `cargo` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+CREATE TABLE aula (
+                id_aula INT NOT NULL,
+                facultad VARCHAR(100) NOT NULL,
+                id_escuela INT NOT NULL,
+                PRIMARY KEY (id_aula)
+);
 
---
--- Estructura de tabla para la tabla `pregunta`
---
 
-CREATE TABLE `pregunta` (
-  `id` int(11) NOT NULL,
-  `enunciado` tinytext NOT NULL,
-  `respuesta` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE pregunta (
+                id_pregunta INT NOT NULL,
+                nombre VARCHAR(100) NOT NULL,
+                titulo VARCHAR(100) NOT NULL,
+                descripcion VARCHAR(100) NOT NULL,
+                respuesta VARCHAR(1) NOT NULL,
+                respuesta_imagen OTHER NOT NULL,
+                examen_id INT NOT NULL,
+                fecha DATE NOT NULL,
+                area_nombre VARCHAR(20) NOT NULL,
+                id_proceso_pregunta INT NOT NULL,
+                PRIMARY KEY (id_pregunta)
+);
 
--- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `proceso`
---
+ALTER TABLE participante_proceso ADD CONSTRAINT proceso_participante_proceso_fk
+FOREIGN KEY (fecha)
+REFERENCES proceso (fecha)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-CREATE TABLE `proceso` (
-  `id` int(15) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `fecha` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE proceso_pregunta ADD CONSTRAINT proceso_proceso_pregunta_fk
+FOREIGN KEY (fecha)
+REFERENCES proceso (fecha)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
--- --------------------------------------------------------
+ALTER TABLE pregunta ADD CONSTRAINT proceso_pregunta_fk
+FOREIGN KEY (fecha)
+REFERENCES proceso (fecha)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
---
--- Estructura de tabla para la tabla `proceso_participante`
---
+ALTER TABLE participante ADD CONSTRAINT proceso_participante_fk
+FOREIGN KEY (fecha)
+REFERENCES proceso (fecha)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-CREATE TABLE `proceso_participante` (
-  `id` int(11) NOT NULL,
-  `id_participante` int(11) NOT NULL,
-  `id_proceso` int(15) NOT NULL,
-  `area` varchar(50) NOT NULL,
-  `escuela` varchar(250) DEFAULT NULL,
-  `aula` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE participante ADD CONSTRAINT detalle_participante_participante_fk
+FOREIGN KEY (id_detalle_participante)
+REFERENCES detalle_participante (id_detalle_participante)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
--- --------------------------------------------------------
+ALTER TABLE participante ADD CONSTRAINT tipo_participante_participante_fk
+FOREIGN KEY (id_tipo_participante)
+REFERENCES tipo_participante (id_tipo_participante)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
---
--- Estructura de tabla para la tabla `proceso_pregunta`
---
+ALTER TABLE proceso_pregunta ADD CONSTRAINT participante_proceso_pregunta_fk
+FOREIGN KEY (dni)
+REFERENCES participante (dni)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-CREATE TABLE `proceso_pregunta` (
-  `id` int(11) NOT NULL,
-  `id_pregunta` int(11) NOT NULL,
-  `id_participante` int(11) NOT NULL,
-  `id_proceso` int(15) NOT NULL,
-  `estado` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE pregunta ADD CONSTRAINT proceso_pregunta_pregunta_fk
+FOREIGN KEY (id_proceso_pregunta)
+REFERENCES proceso_pregunta (id_proceso_pregunta)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
--- --------------------------------------------------------
+ALTER TABLE area ADD CONSTRAINT proceso_pregunta_area_fk
+FOREIGN KEY (id_proceso_pregunta)
+REFERENCES proceso_pregunta (id_proceso_pregunta)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
---
--- Estructura de tabla para la tabla `tipo_participante`
---
+ALTER TABLE pregunta ADD CONSTRAINT area_pregunta_fk
+FOREIGN KEY (area_nombre)
+REFERENCES area (nombre)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-CREATE TABLE `tipo_participante` (
-  `cargo` varchar(50) NOT NULL,
-  `tipo` varchar(50) NOT NULL,
-  `funcion` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE escuela ADD CONSTRAINT area_escuela_fk
+FOREIGN KEY (area_nombre)
+REFERENCES area (nombre)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
--- --------------------------------------------------------
+ALTER TABLE participante_proceso ADD CONSTRAINT area_participante_proceso_fk
+FOREIGN KEY (nombre)
+REFERENCES area (nombre)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
---
--- Estructura de tabla para la tabla `usuario`
---
+ALTER TABLE participante ADD CONSTRAINT participante_proceso_participante_fk
+FOREIGN KEY (id_participante_proceso)
+REFERENCES participante_proceso (id_participante_proceso)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-CREATE TABLE `usuario` (
-  `dni` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `correo` varchar(255) NOT NULL,
-  `usuario` varchar(255) NOT NULL,
-  `con` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`dni`, `nombre`, `correo`, `usuario`, `con`) VALUES
-(11111, 'franco', 'franco@gmail.com', 'dui', 'dui');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `area`
---
-ALTER TABLE `area`
-  ADD PRIMARY KEY (`area`);
-
---
--- Indices de la tabla `aula`
---
-ALTER TABLE `aula`
-  ADD PRIMARY KEY (`numero`),
-  ADD KEY `escuela` (`escuela`);
-
---
--- Indices de la tabla `escuela`
---
-ALTER TABLE `escuela`
-  ADD PRIMARY KEY (`nombre`),
-  ADD KEY `area` (`area`);
-
---
--- Indices de la tabla `participante`
---
-ALTER TABLE `participante`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cargo` (`cargo`);
-
---
--- Indices de la tabla `pregunta`
---
-ALTER TABLE `pregunta`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `proceso`
---
-ALTER TABLE `proceso`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `proceso_participante`
---
-ALTER TABLE `proceso_participante`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `area` (`area`),
-  ADD KEY `aula` (`aula`),
-  ADD KEY `escuela` (`escuela`),
-  ADD KEY `id_proceso` (`id_proceso`),
-  ADD KEY `id_participante` (`id_participante`);
-
---
--- Indices de la tabla `proceso_pregunta`
---
-ALTER TABLE `proceso_pregunta`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_pregunta` (`id_pregunta`),
-  ADD KEY `id_participante` (`id_participante`),
-  ADD KEY `id_proceso` (`id_proceso`);
-
---
--- Indices de la tabla `tipo_participante`
---
-ALTER TABLE `tipo_participante`
-  ADD PRIMARY KEY (`cargo`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`dni`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `participante`
---
-ALTER TABLE `participante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `pregunta`
---
-ALTER TABLE `pregunta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `proceso`
---
-ALTER TABLE `proceso`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `proceso_participante`
---
-ALTER TABLE `proceso_participante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `proceso_pregunta`
---
-ALTER TABLE `proceso_pregunta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `aula`
---
-ALTER TABLE `aula`
-  ADD CONSTRAINT `aula_ibfk_1` FOREIGN KEY (`escuela`) REFERENCES `escuela` (`nombre`);
-
---
--- Filtros para la tabla `escuela`
---
-ALTER TABLE `escuela`
-  ADD CONSTRAINT `escuela_ibfk_1` FOREIGN KEY (`area`) REFERENCES `area` (`area`);
-
---
--- Filtros para la tabla `participante`
---
-ALTER TABLE `participante`
-  ADD CONSTRAINT `participante_ibfk_1` FOREIGN KEY (`cargo`) REFERENCES `tipo_participante` (`cargo`);
-
---
--- Filtros para la tabla `proceso_participante`
---
-ALTER TABLE `proceso_participante`
-  ADD CONSTRAINT `proceso_participante_ibfk_1` FOREIGN KEY (`area`) REFERENCES `area` (`area`),
-  ADD CONSTRAINT `proceso_participante_ibfk_2` FOREIGN KEY (`aula`) REFERENCES `aula` (`numero`),
-  ADD CONSTRAINT `proceso_participante_ibfk_3` FOREIGN KEY (`escuela`) REFERENCES `escuela` (`nombre`),
-  ADD CONSTRAINT `proceso_participante_ibfk_4` FOREIGN KEY (`id_proceso`) REFERENCES `proceso` (`id`),
-  ADD CONSTRAINT `proceso_participante_ibfk_5` FOREIGN KEY (`id_participante`) REFERENCES `participante` (`id`);
-
---
--- Filtros para la tabla `proceso_pregunta`
---
-ALTER TABLE `proceso_pregunta`
-  ADD CONSTRAINT `proceso_pregunta_ibfk_1` FOREIGN KEY (`id_pregunta`) REFERENCES `pregunta` (`id`),
-  ADD CONSTRAINT `proceso_pregunta_ibfk_2` FOREIGN KEY (`id_participante`) REFERENCES `participante` (`id`),
-  ADD CONSTRAINT `proceso_pregunta_ibfk_3` FOREIGN KEY (`id_proceso`) REFERENCES `proceso` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE aula ADD CONSTRAINT escuela_aula_fk
+FOREIGN KEY (id_escuela)
+REFERENCES escuela (id_escuela)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
