5ad490c0e0cebdb6e6ce4d2a4dce2a435891e5e9
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2017-07-15 13:20:41 -0500

    Update internetProjectDataBase.sql

--- internetProjectDataBase.sql
+++ internetProjectDataBase.sql
@@ -1,171 +1,165 @@
+CREATE TABLE `area` (
+  `nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE usuario (
-                id_usuario INT NOT NULL,
-                nombre VARCHAR(100) NOT NULL,
-                apellido VARCHAR(100) NOT NULL,
-                correo VARCHAR(50) NOT NULL,
-                PRIMARY KEY (id_usuario)
-);
+CREATE TABLE `aula` (
+  `id_aula` int(11) NOT NULL,
+  `facultad` varchar(100) NOT NULL,
+  `id_escuela` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `detalle_participante` (
+  `id_detalle_participante` int(11) NOT NULL,
+  `facultad` varchar(100) NOT NULL,
+  `regimen` varchar(100) NOT NULL,
+  `dependencia` varchar(100) NOT NULL,
+  `id_participante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE area (
-                nombre VARCHAR(20) NOT NULL,
-                PRIMARY KEY (nombre)
-);
+CREATE TABLE `escuela` (
+  `id_escuela` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `area_nombre` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `participante` (
+  `id_participante` int(11) NOT NULL,
+  `dni` varchar(8) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `correo` varchar(30) NOT NULL,
+  `tipo_nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE escuela (
-                id_escuela INT AUTO_INCREMENT NOT NULL,
-                nombre VARCHAR(30) NOT NULL,
-                area_nombre VARCHAR(20) NOT NULL,
-                PRIMARY KEY (id_escuela)
-);
+CREATE TABLE `pregunta` (
+  `id_pregunta` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `respuesta` varchar(1) NOT NULL,
+  `respuesta_imagen` varchar(500) NOT NULL,
+  `id_participante` int(11) NOT NULL,
+  `id_proceso` varchar(100) DEFAULT NULL,
+  `a` varchar(150) NOT NULL,
+  `b` varchar(150) NOT NULL,
+  `c` varchar(150) NOT NULL,
+  `d` varchar(150) NOT NULL,
+  `e` varchar(150) NOT NULL,
+  `estado` char(1) NOT NULL,
+  `area` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `proceso` (
+  `id_proceso` varchar(100) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE aula (
-                id_aula INT AUTO_INCREMENT NOT NULL,
-                facultad VARCHAR(100) NOT NULL,
-                id_escuela INT NOT NULL,
-                PRIMARY KEY (id_aula)
-);
+CREATE TABLE `proceso_participante` (
+  `id_proceso_participante` int(11) NOT NULL,
+  `id_proceso` varchar(100) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `id_participante` int(11) NOT NULL,
+  `id_aula` int(11) DEFAULT NULL,
+  `id_escuela` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE `tipo_participacion` (
+  `id_tipo_participacion` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL,
+  `id_participante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE proceso (
-                id_proceso VARCHAR(100) NOT NULL,
-                nombre VARCHAR(100) NOT NULL,
-                fecha DATE NOT NULL,
-                PRIMARY KEY (id_proceso)
-);
+CREATE TABLE `tipo_participante` (
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-CREATE TABLE examen (
-                id_examen INT NOT NULL,
-                descripcion VARCHAR(100) NOT NULL,
-                id_proceso VARCHAR(100) NOT NULL,
-                PRIMARY KEY (id_examen)
-);
-
-
-CREATE TABLE pregunta (
-                id_pregunta INT AUTO_INCREMENT NOT NULL,
-                nombre VARCHAR(100) NOT NULL,
-                titulo VARCHAR(100) NOT NULL,
-                descripcion VARCHAR(100) NOT NULL,
-                respuesta VARCHAR(1) NOT NULL,
-                respuesta_imagen VARCHAR(500) NOT NULL,
-                examen_id INT NOT NULL,
-                id_examen INT NOT NULL,
-                a VARCHAR(1) NOT NULL,
-                b VARCHAR(1) NOT NULL,
-                c VARCHAR(1) NOT NULL,
-                d VARCHAR(1) NOT NULL,
-                e VARCHAR(1) NOT NULL,
-                PRIMARY KEY (id_pregunta)
-);
-
-
-CREATE TABLE participante (
-                id_participante INT NOT NULL,
-                dni VARCHAR(8) NOT NULL,
-                nombre VARCHAR(100) NOT NULL,
-                apellido VARCHAR(100) NOT NULL,
-                telefono VARCHAR(10) NOT NULL,
-                correo VARCHAR(30) NOT NULL,
-                PRIMARY KEY (id_participante)
-);
-
-
-CREATE TABLE tipo_participante (
-                id_tipo_participante INT AUTO_INCREMENT NOT NULL,
-                nombre VARCHAR(100) NOT NULL,
-                id_participante INT NOT NULL,
-                PRIMARY KEY (id_tipo_participante)
-);
-
-
-CREATE TABLE detalle_participante (
-                id_detalle_participante INT AUTO_INCREMENT NOT NULL,
-                facultad VARCHAR(100) NOT NULL,
-                regimen VARCHAR(100) NOT NULL,
-                dependencia VARCHAR(100) NOT NULL,
-                id_participante INT NOT NULL,
-                PRIMARY KEY (id_detalle_participante)
-);
+CREATE TABLE `usuario` (
+  `dni` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `correo` varchar(255) NOT NULL,
+  `usuario` varchar(255) NOT NULL,
+  `con` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-CREATE TABLE proceso_participante (
-                id_proceso_participante INT NOT NULL,
-                id_proceso VARCHAR(100) NOT NULL,
-                nombre VARCHAR(20) NOT NULL,
-                id_participante INT NOT NULL,
-                PRIMARY KEY (id_proceso_participante)
-);
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`nombre`);
+
+ALTER TABLE `aula`
+  ADD PRIMARY KEY (`id_aula`),
+  ADD KEY `escuela_aula_fk` (`id_escuela`);
+
+ALTER TABLE `detalle_participante`
+  ADD PRIMARY KEY (`id_detalle_participante`),
+  ADD KEY `participante_detalle_participante_fk` (`id_participante`);
+
+ALTER TABLE `escuela`
+  ADD PRIMARY KEY (`id_escuela`),
+  ADD KEY `area_escuela_fk` (`area_nombre`);
+
+ALTER TABLE `participante`
+  ADD PRIMARY KEY (`id_participante`),
+  ADD KEY `nombre_tipo_fk` (`tipo_nombre`);
+
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`id_pregunta`),
+  ADD KEY `id_participante_fk` (`id_participante`),
+  ADD KEY `id_proceso_fk` (`id_proceso`),
+  ADD KEY `area_fk` (`area`);
+
+ALTER TABLE `proceso`
+  ADD PRIMARY KEY (`id_proceso`);
+
+ALTER TABLE `proceso_participante`
+  ADD PRIMARY KEY (`id_proceso_participante`),
+  ADD KEY `area_proceso_participante_fk` (`nombre`),
+  ADD KEY `proceso_proceso_participante_fk` (`id_proceso`),
+  ADD KEY `participante_proceso_participante_fk` (`id_participante`),
+  ADD KEY `id_aula_fk` (`id_aula`),
+  ADD KEY `id_escuela_fka` (`id_escuela`);
+
+ALTER TABLE `tipo_participacion`
+  ADD PRIMARY KEY (`id_tipo_participacion`),
+  ADD KEY `participante_tipo_participacion_fk` (`id_participante`);
+
+ALTER TABLE `tipo_participante`
+  ADD PRIMARY KEY (`nombre`);
 
 
-CREATE TABLE tipo_participacion (
-                id_tipo_participacion INT NOT NULL,
-                nombre VARCHAR(20) NOT NULL,
-                id_participante INT NOT NULL,
-                PRIMARY KEY (id_tipo_participacion)
-);
+ALTER TABLE `aula`
+  MODIFY `id_aula` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `detalle_participante`
+  MODIFY `id_detalle_participante` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `escuela`
+  MODIFY `id_escuela` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `pregunta`
+  MODIFY `id_pregunta` int(11) NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `aula`
+  ADD CONSTRAINT `escuela_aula_fk` FOREIGN KEY (`id_escuela`) REFERENCES `escuela` (`id_escuela`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE escuela ADD CONSTRAINT area_escuela_fk
-FOREIGN KEY (area_nombre)
-REFERENCES area (nombre)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ALTER TABLE `detalle_participante`
+  ADD CONSTRAINT `participante_detalle_participante_fk` FOREIGN KEY (`id_participante`) REFERENCES `participante` (`id_participante`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE proceso_participante ADD CONSTRAINT area_proceso_participante_fk
-FOREIGN KEY (nombre)
-REFERENCES area (nombre)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ALTER TABLE `escuela`
+  ADD CONSTRAINT `area_escuela_fk` FOREIGN KEY (`area_nombre`) REFERENCES `area` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE aula ADD CONSTRAINT escuela_aula_fk
-FOREIGN KEY (id_escuela)
-REFERENCES escuela (id_escuela)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ALTER TABLE `participante`
+  ADD CONSTRAINT `nombre_tipo_fk` FOREIGN KEY (`tipo_nombre`) REFERENCES `tipo_participante` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE examen ADD CONSTRAINT proceso_examen_fk
-FOREIGN KEY (id_proceso)
-REFERENCES proceso (id_proceso)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ALTER TABLE `pregunta`
+  ADD CONSTRAINT `area_fk` FOREIGN KEY (`area`) REFERENCES `area` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `id_participante_fk` FOREIGN KEY (`id_participante`) REFERENCES `participante` (`id_participante`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `id_proceso_fk` FOREIGN KEY (`id_proceso`) REFERENCES `proceso` (`id_proceso`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE proceso_participante ADD CONSTRAINT proceso_proceso_participante_fk
-FOREIGN KEY (id_proceso)
-REFERENCES proceso (id_proceso)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ALTER TABLE `proceso_participante`
+  ADD CONSTRAINT `area_proceso_participante_fk` FOREIGN KEY (`nombre`) REFERENCES `area` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `id_aula_fk` FOREIGN KEY (`id_aula`) REFERENCES `aula` (`id_aula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `id_escuela_fka` FOREIGN KEY (`id_escuela`) REFERENCES `escuela` (`id_escuela`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `participante_proceso_participante_fk` FOREIGN KEY (`id_participante`) REFERENCES `participante` (`id_participante`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `proceso_proceso_participante_fk` FOREIGN KEY (`id_proceso`) REFERENCES `proceso` (`id_proceso`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-ALTER TABLE pregunta ADD CONSTRAINT examen_pregunta_fk
-FOREIGN KEY (id_examen)
-REFERENCES examen (id_examen)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE tipo_participacion ADD CONSTRAINT participante_tipo_participacion_fk
-FOREIGN KEY (id_participante)
-REFERENCES participante (id_participante)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE proceso_participante ADD CONSTRAINT participante_proceso_participante_fk
-FOREIGN KEY (id_participante)
-REFERENCES participante (id_participante)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE detalle_participante ADD CONSTRAINT participante_detalle_participante_fk
-FOREIGN KEY (id_participante)
-REFERENCES participante (id_participante)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
-
-ALTER TABLE tipo_participante ADD CONSTRAINT participante_tipo_participante_fk
-FOREIGN KEY (id_participante)
-REFERENCES participante (id_participante)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION;
+ALTER TABLE `tipo_participacion`
+  ADD CONSTRAINT `participante_tipo_participacion_fk` FOREIGN KEY (`id_participante`) REFERENCES `participante` (`id_participante`) ON DELETE NO ACTION ON UPDATE NO ACTION;
