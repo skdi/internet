f9a341623e6e8b44feee818518b0f6351a5d0e2c
MBlev <mb.lev17@gmail.com>
2017-07-24 02:16:09 -0500

    Seleccionar proceso - Exportar

--- sistema_web_cascara/exportar/exportar.php
+++ sistema_web_cascara/exportar/exportar.php
@@ -1,10 +1,11 @@
 <?php
-  $tabla=$_POST['tabla'];
-  $formato=$_POST['formato'];
+  $proceso=$_POST["proceso"];
+  $formato=$_POST["formato"];
   $connect=mysqli_connect("localhost","root","","proyecto");
   $output='';
   if(isset($_POST["export"])){
-    $query="SELECT *FROM "."$tabla";
+    $query="SELECT participante.dni, participante.nombre, participante.apellido FROM participante, proceso_participante WHERE participante.id_participante=proceso_participante.id_participante AND proceso_participante.id_proceso="."$proceso"; 
+    //$query="SELECT *FROM "."$tabla";
     $result=mysqli_query($connect,$query); 
     if (mysqli_num_rows($result)>0) {
       $output .= '
@@ -13,36 +14,25 @@
           <th>Edad</th>
           <th>Nombre</th>
           <th>Apellido</th>
-          <th>Facultad</th>
-          <th>Teléfono</th>
-          <th>Correo</th>
-          <th>Categoria</th>
-          <th>Régimen</th>
-          <th>Cargo</th>
-        </tr>
       ';
       while($row=mysqli_fetch_array($result)){
         $output.='
           <tr>
             <td>'.$row["dni"].'</td>
             <td>'.$row["nombre"].'</td>
-            <td>'.$row["apellidos"].'</td>
-            <td>'.$row["facultad"].'</td>
-            <td>'.$row["telefono"].'</td>
-            <td>'.$row["correo"].'</td>
-            <td>'.$row["categoria"].'</td>
-            <td>'.$row["regimen"].'</td>
-            <td>'.$row["cargo"].'</td>
+            <td>'.$row["apellido"].'</td>
           </tr>
         ';
       }
       $output .='</table>';
+
       header('Content-Type: application/xls');
       header('Content-Disposition: attachment; filename=download.xls');
       echo $output;
     }
     else{
-      echo "<center><h4>No hay datos almacenados</h4></center>"
+      echo "<center><h4>No hay datos almacenados</h4></center>";
     }
   }
 ?>
+
