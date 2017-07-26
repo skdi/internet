13a6cdbf19e492cd670697ba68afa23cbad0d69c
Skdi <jddhghf@gmail.com>
2017-07-25 23:22:28 -0500

    Update crear.php

--- proyecto-funcional/mantenimiento/escuelas/clases/crear.php
+++ proyecto-funcional/mantenimiento/escuelas/clases/crear.php
@@ -1,35 +1,28 @@
 <?php
-require_once("participante.php");
+require_once("escuela.php");
 session_start();
 
-$dni= $_POST['dni'];
+$area_nombre= $_POST['area_nombre'];
 $nombre= $_POST['nombre'];
-$apellido= $_POST['apellido'];
-$correo= $_POST['correo'];
-$telefono= $_POST['telefono'];
-$participacion= $_POST['participacion'];
 
 
-$categoria=$_POST['categoria'];
-$fc=$_POST['fc'];
-$regi=$_POST['rg'];
 
-$participante= new participante('',$dni,$nombre,$apellido,$telefono,$correo,'Docente',0,$participacion,'a');
-$resultado=$participante->crear();
+$escuela= new escuela('',$area_nombre,$nombre);
+$resultado=$escuela->crear();
 
 $enlace= new conectar();
 $conexion= $enlace->conexion;
 
 if($resultado)
 {
-    $sql1="Select id_participante from participante where dni='$dni'";
+    $sql1="Select id_escuela from escuela where area_nombre='$area_nombre'";
     $datos=mysqli_fetch_array(mysqli_query($conexion,$sql1));
-    $id=$datos['id_participante'];
+    $id=$datos['id_escuela'];
     
 }
 
 
-$sql="INSERT INTO detalle_participante (facultad,regimen,dependencia,id_participante,categoria) VALUES ('$fc','$regi','','$id','$categoria')";
+$sql="INSERT INTO escuela (nombre,area_nombre) VALUES ('$nombre','$area_nombre')";
 
 
 $resultado1=mysqli_query($conexion,$sql); 
