5d84c171b4b0a3299e4b72eadc9d38e366b786d5
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2017-07-26 00:59:42 -0500

    Update columnas.php

--- proyecto/seleccionar/columnas.php
+++ proyecto/seleccionar/columnas.php
@@ -1,19 +1,20 @@
+
 <?php
 //Include database configuration file
-include('base.php');
-
+include('../clases/conexion/conexion.php');
+$con=$conexion;
 if(isset($_POST["nombre"]) && !empty($_POST["nombre"])){
     //Get all state data
     $area=$_POST['nombre'];
     
-    $query = $db->query("SELECT * FROM escuela WHERE area_nombre = '".$area."' ORDER BY nombre ASC");
+    $query = mysqli_query($con,"SELECT * FROM escuela WHERE area_nombre = '".$area."' ORDER BY nombre ASC");
     
     //Count total number of rows
-    $rowCount = $query->num_rows;
+    $rowCount = mysqli_num_fields($query);
     //Display states list
     if($rowCount > 0){
         echo '<option value="">Selecciona escuela</option>';
-        while($row = $query->fetch_assoc()){ 
+        while($row = mysqli_fetch_array($query)){ 
             echo '<option value="'.$row['id_escuela'].'">'.$row['nombre'].'</option>';
         }
     }else{
@@ -23,15 +24,15 @@ if(isset($_POST["nombre"]) && !empty($_POST["nombre"])){
 
 if(isset($_POST["id_escuela"]) && !empty($_POST["id_escuela"])){
     //Get all city data
-    $query = $db->query("SELECT * FROM aula WHERE id_escuela = ".$_POST['id_escuela']." ORDER BY n_aula ASC");
+    $query = mysqli_query($con,"SELECT * FROM aula WHERE id_escuela = ".$_POST['id_escuela']." ORDER BY n_aula ASC");
     
     //Count total number of rows
-    $rowCount = $query->num_rows;
+    $rowCount = mysqli_num_fields($query);
     
     //Display cities list
     if($rowCount > 0){
         echo '<option value="">Selcciona aula</option>';
-        while($row = $query->fetch_assoc()){ 
+        while($row = mysqli_fetch_array($query)){ 
             echo '<option value="'.$row['id_aula'].'">'.$row['n_aula'].'</option>';
         }
     }else{
